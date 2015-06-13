bf176fee03640148488930b363a2833e932ed3c6
Conflei <paramaplexd@gmail.com>
2015-06-13 15:07:06 -0430

    Scroll Botttom ATI Example

---
+++ [ATI] jQuery - Scroll Bottom Example/js/loadelements.js
@@ -0,0 +1,204 @@
+var misObjetos = [
+  {
+  	"id" : 1,
+    "title" : "Dolor sit",
+    "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget justo mattis ipsum finibus fringilla quis non eros. Donec nunc magna, maximus non eros quis, dignissim dapibus quam. Donec hendrerit sapien quis ex feugiat, nec rutrum elit pharetra. Phasellus tempor eleifend elementum. Fusce posuere nec nunc eget pretium. Morbi nec posuere elit, sed sollicitudin elit. Quisque et mauris ac elit rutrum maximus vehicula quis risus. Donec sed posuere diam.",
+    "url" : "https://fbcdn-sphotos-g-a.akamaihd.net/hphotos-ak-xat1/v/t1.0-9/s720x720/11403403_10204499349949341_8536796169825131204_n.jpg?oh=746a623f117d7a6ce268223343943554&oe=55EF8756&__gda__=1441544023_bccb7797ca6bf15277f4b71d6f95dc5a"
+  },
+  {
+    "id" : 2,
+    "title" : "Dolor sit",
+    "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget justo mattis ipsum finibus fringilla quis non eros. Donec nunc magna, maximus non eros quis, dignissim dapibus quam. Donec hendrerit sapien quis ex feugiat, nec rutrum elit pharetra. Phasellus tempor eleifend elementum. Fusce posuere nec nunc eget pretium. Morbi nec posuere elit, sed sollicitudin elit. Quisque et mauris ac elit rutrum maximus vehicula quis risus. Donec sed posuere diam.",
+    "url" : "https://fbcdn-sphotos-a-a.akamaihd.net/hphotos-ak-xta1/v/t1.0-9/s720x720/11390122_10204499348509305_2065362707270282665_n.jpg?oh=725694c4e12025f08b6509c6e9c73931&oe=55F1DB63&__gda__=1446178930_7c6353e101f33a930d00893fc5116290"
+  },
+  {
+    "id" : 3,
+    "title" : "Dolor sit",
+    "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget justo mattis ipsum finibus fringilla quis non eros. Donec nunc magna, maximus non eros quis, dignissim dapibus quam. Donec hendrerit sapien quis ex feugiat, nec rutrum elit pharetra. Phasellus tempor eleifend elementum. Fusce posuere nec nunc eget pretium. Morbi nec posuere elit, sed sollicitudin elit. Quisque et mauris ac elit rutrum maximus vehicula quis risus. Donec sed posuere diam.",
+    "url" : "https://fbcdn-sphotos-h-a.akamaihd.net/hphotos-ak-xaf1/v/t1.0-9/11391268_10204499347709285_3889169831865952197_n.jpg?oh=7f3cf692d4dca830c835e0c9024dbc96&oe=55F4D3D2&__gda__=1442616678_b8b15ac6a8b75419a593cc611e190e95"
+  },
+  {
+    "id" : 4,
+    "title" : "Dolor sit",
+    "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget justo mattis ipsum finibus fringilla quis non eros. Donec nunc magna, maximus non eros quis, dignissim dapibus quam. Donec hendrerit sapien quis ex feugiat, nec rutrum elit pharetra. Phasellus tempor eleifend elementum. Fusce posuere nec nunc eget pretium. Morbi nec posuere elit, sed sollicitudin elit. Quisque et mauris ac elit rutrum maximus vehicula quis risus. Donec sed posuere diam.",
+    "url" : "https://fbcdn-sphotos-b-a.akamaihd.net/hphotos-ak-xft1/v/t1.0-9/11390378_10204499347029268_9202303841252452016_n.jpg?oh=fe8a27b81282af40fd6103e637d6e7a7&oe=56066274&__gda__=1443172486_44f574be25879517ecee37c62ecd6f05"
+  },
+  {
+    "id" : 5,
+    "title" : "Dolor sit",
+    "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget justo mattis ipsum finibus fringilla quis non eros. Donec nunc magna, maximus non eros quis, dignissim dapibus quam. Donec hendrerit sapien quis ex feugiat, nec rutrum elit pharetra. Phasellus tempor eleifend elementum. Fusce posuere nec nunc eget pretium. Morbi nec posuere elit, sed sollicitudin elit. Quisque et mauris ac elit rutrum maximus vehicula quis risus. Donec sed posuere diam.",
+    "url" : "https://fbcdn-sphotos-a-a.akamaihd.net/hphotos-ak-xpt1/t31.0-8/s720x720/1556283_10204499345669234_8621539168670781182_o.jpg"
+  },
+  {
+    "id" : 6,
+    "title" : "Dolor sit",
+    "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget justo mattis ipsum finibus fringilla quis non eros. Donec nunc magna, maximus non eros quis, dignissim dapibus quam. Donec hendrerit sapien quis ex feugiat, nec rutrum elit pharetra. Phasellus tempor eleifend elementum. Fusce posuere nec nunc eget pretium. Morbi nec posuere elit, sed sollicitudin elit. Quisque et mauris ac elit rutrum maximus vehicula quis risus. Donec sed posuere diam.",
+    "url" : "https://fbcdn-sphotos-g-a.akamaihd.net/hphotos-ak-xat1/v/t1.0-9/s720x720/11403403_10204499349949341_8536796169825131204_n.jpg?oh=746a623f117d7a6ce268223343943554&oe=55EF8756&__gda__=1441544023_bccb7797ca6bf15277f4b71d6f95dc5a"
+  },
+  {
+    "id" : 7,
+    "title" : "Dolor sit",
+    "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget justo mattis ipsum finibus fringilla quis non eros. Donec nunc magna, maximus non eros quis, dignissim dapibus quam. Donec hendrerit sapien quis ex feugiat, nec rutrum elit pharetra. Phasellus tempor eleifend elementum. Fusce posuere nec nunc eget pretium. Morbi nec posuere elit, sed sollicitudin elit. Quisque et mauris ac elit rutrum maximus vehicula quis risus. Donec sed posuere diam.",
+    "url" : "https://fbcdn-sphotos-a-a.akamaihd.net/hphotos-ak-xta1/v/t1.0-9/s720x720/11390122_10204499348509305_2065362707270282665_n.jpg?oh=725694c4e12025f08b6509c6e9c73931&oe=55F1DB63&__gda__=1446178930_7c6353e101f33a930d00893fc5116290"
+  },
+  {
+    "id" : 8,
+    "title" : "Dolor sit",
+    "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget justo mattis ipsum finibus fringilla quis non eros. Donec nunc magna, maximus non eros quis, dignissim dapibus quam. Donec hendrerit sapien quis ex feugiat, nec rutrum elit pharetra. Phasellus tempor eleifend elementum. Fusce posuere nec nunc eget pretium. Morbi nec posuere elit, sed sollicitudin elit. Quisque et mauris ac elit rutrum maximus vehicula quis risus. Donec sed posuere diam.",
+    "url" : "https://fbcdn-sphotos-h-a.akamaihd.net/hphotos-ak-xaf1/v/t1.0-9/11391268_10204499347709285_3889169831865952197_n.jpg?oh=7f3cf692d4dca830c835e0c9024dbc96&oe=55F4D3D2&__gda__=1442616678_b8b15ac6a8b75419a593cc611e190e95"
+  },
+  {
+    "id" : 9,
+    "title" : "Dolor sit",
+    "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget justo mattis ipsum finibus fringilla quis non eros. Donec nunc magna, maximus non eros quis, dignissim dapibus quam. Donec hendrerit sapien quis ex feugiat, nec rutrum elit pharetra. Phasellus tempor eleifend elementum. Fusce posuere nec nunc eget pretium. Morbi nec posuere elit, sed sollicitudin elit. Quisque et mauris ac elit rutrum maximus vehicula quis risus. Donec sed posuere diam.",
+    "url" : "https://fbcdn-sphotos-b-a.akamaihd.net/hphotos-ak-xft1/v/t1.0-9/11390378_10204499347029268_9202303841252452016_n.jpg?oh=fe8a27b81282af40fd6103e637d6e7a7&oe=56066274&__gda__=1443172486_44f574be25879517ecee37c62ecd6f05"
+  },
+  {
+    "id" : 10,
+    "title" : "Dolor sit",
+    "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget justo mattis ipsum finibus fringilla quis non eros. Donec nunc magna, maximus non eros quis, dignissim dapibus quam. Donec hendrerit sapien quis ex feugiat, nec rutrum elit pharetra. Phasellus tempor eleifend elementum. Fusce posuere nec nunc eget pretium. Morbi nec posuere elit, sed sollicitudin elit. Quisque et mauris ac elit rutrum maximus vehicula quis risus. Donec sed posuere diam.",
+    "url" : "https://fbcdn-sphotos-a-a.akamaihd.net/hphotos-ak-xpt1/t31.0-8/s720x720/1556283_10204499345669234_8621539168670781182_o.jpg"
+  },
+  {
+    "id" : 11,
+    "title" : "Dolor sit",
+    "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget justo mattis ipsum finibus fringilla quis non eros. Donec nunc magna, maximus non eros quis, dignissim dapibus quam. Donec hendrerit sapien quis ex feugiat, nec rutrum elit pharetra. Phasellus tempor eleifend elementum. Fusce posuere nec nunc eget pretium. Morbi nec posuere elit, sed sollicitudin elit. Quisque et mauris ac elit rutrum maximus vehicula quis risus. Donec sed posuere diam.",
+    "url" : "https://fbcdn-sphotos-g-a.akamaihd.net/hphotos-ak-xat1/v/t1.0-9/s720x720/11403403_10204499349949341_8536796169825131204_n.jpg?oh=746a623f117d7a6ce268223343943554&oe=55EF8756&__gda__=1441544023_bccb7797ca6bf15277f4b71d6f95dc5a"
+  },
+  {
+    "id" : 12,
+    "title" : "Dolor sit",
+    "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget justo mattis ipsum finibus fringilla quis non eros. Donec nunc magna, maximus non eros quis, dignissim dapibus quam. Donec hendrerit sapien quis ex feugiat, nec rutrum elit pharetra. Phasellus tempor eleifend elementum. Fusce posuere nec nunc eget pretium. Morbi nec posuere elit, sed sollicitudin elit. Quisque et mauris ac elit rutrum maximus vehicula quis risus. Donec sed posuere diam.",
+    "url" : "https://fbcdn-sphotos-a-a.akamaihd.net/hphotos-ak-xta1/v/t1.0-9/s720x720/11390122_10204499348509305_2065362707270282665_n.jpg?oh=725694c4e12025f08b6509c6e9c73931&oe=55F1DB63&__gda__=1446178930_7c6353e101f33a930d00893fc5116290"
+  },
+  {
+    "id" : 13,
+    "title" : "Dolor sit",
+    "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget justo mattis ipsum finibus fringilla quis non eros. Donec nunc magna, maximus non eros quis, dignissim dapibus quam. Donec hendrerit sapien quis ex feugiat, nec rutrum elit pharetra. Phasellus tempor eleifend elementum. Fusce posuere nec nunc eget pretium. Morbi nec posuere elit, sed sollicitudin elit. Quisque et mauris ac elit rutrum maximus vehicula quis risus. Donec sed posuere diam.",
+    "url" : "https://fbcdn-sphotos-h-a.akamaihd.net/hphotos-ak-xaf1/v/t1.0-9/11391268_10204499347709285_3889169831865952197_n.jpg?oh=7f3cf692d4dca830c835e0c9024dbc96&oe=55F4D3D2&__gda__=1442616678_b8b15ac6a8b75419a593cc611e190e95"
+  },
+  {
+    "id" : 14,
+    "title" : "Dolor sit",
+    "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget justo mattis ipsum finibus fringilla quis non eros. Donec nunc magna, maximus non eros quis, dignissim dapibus quam. Donec hendrerit sapien quis ex feugiat, nec rutrum elit pharetra. Phasellus tempor eleifend elementum. Fusce posuere nec nunc eget pretium. Morbi nec posuere elit, sed sollicitudin elit. Quisque et mauris ac elit rutrum maximus vehicula quis risus. Donec sed posuere diam.",
+    "url" : "https://fbcdn-sphotos-b-a.akamaihd.net/hphotos-ak-xft1/v/t1.0-9/11390378_10204499347029268_9202303841252452016_n.jpg?oh=fe8a27b81282af40fd6103e637d6e7a7&oe=56066274&__gda__=1443172486_44f574be25879517ecee37c62ecd6f05"
+  },
+  {
+    "id" : 15,
+    "title" : "Dolor sit",
+    "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget justo mattis ipsum finibus fringilla quis non eros. Donec nunc magna, maximus non eros quis, dignissim dapibus quam. Donec hendrerit sapien quis ex feugiat, nec rutrum elit pharetra. Phasellus tempor eleifend elementum. Fusce posuere nec nunc eget pretium. Morbi nec posuere elit, sed sollicitudin elit. Quisque et mauris ac elit rutrum maximus vehicula quis risus. Donec sed posuere diam.",
+    "url" : "https://fbcdn-sphotos-a-a.akamaihd.net/hphotos-ak-xpt1/t31.0-8/s720x720/1556283_10204499345669234_8621539168670781182_o.jpg"
+  },
+  {
+    "id" : 16,
+    "title" : "Dolor sit",
+    "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget justo mattis ipsum finibus fringilla quis non eros. Donec nunc magna, maximus non eros quis, dignissim dapibus quam. Donec hendrerit sapien quis ex feugiat, nec rutrum elit pharetra. Phasellus tempor eleifend elementum. Fusce posuere nec nunc eget pretium. Morbi nec posuere elit, sed sollicitudin elit. Quisque et mauris ac elit rutrum maximus vehicula quis risus. Donec sed posuere diam.",
+    "url" : "https://fbcdn-sphotos-g-a.akamaihd.net/hphotos-ak-xat1/v/t1.0-9/s720x720/11403403_10204499349949341_8536796169825131204_n.jpg?oh=746a623f117d7a6ce268223343943554&oe=55EF8756&__gda__=1441544023_bccb7797ca6bf15277f4b71d6f95dc5a"
+  },
+  {
+    "id" : 17,
+    "title" : "Dolor sit",
+    "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget justo mattis ipsum finibus fringilla quis non eros. Donec nunc magna, maximus non eros quis, dignissim dapibus quam. Donec hendrerit sapien quis ex feugiat, nec rutrum elit pharetra. Phasellus tempor eleifend elementum. Fusce posuere nec nunc eget pretium. Morbi nec posuere elit, sed sollicitudin elit. Quisque et mauris ac elit rutrum maximus vehicula quis risus. Donec sed posuere diam.",
+    "url" : "https://fbcdn-sphotos-a-a.akamaihd.net/hphotos-ak-xta1/v/t1.0-9/s720x720/11390122_10204499348509305_2065362707270282665_n.jpg?oh=725694c4e12025f08b6509c6e9c73931&oe=55F1DB63&__gda__=1446178930_7c6353e101f33a930d00893fc5116290"
+  },
+  {
+    "id" : 18,
+    "title" : "Dolor sit",
+    "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget justo mattis ipsum finibus fringilla quis non eros. Donec nunc magna, maximus non eros quis, dignissim dapibus quam. Donec hendrerit sapien quis ex feugiat, nec rutrum elit pharetra. Phasellus tempor eleifend elementum. Fusce posuere nec nunc eget pretium. Morbi nec posuere elit, sed sollicitudin elit. Quisque et mauris ac elit rutrum maximus vehicula quis risus. Donec sed posuere diam.",
+    "url" : "https://fbcdn-sphotos-h-a.akamaihd.net/hphotos-ak-xaf1/v/t1.0-9/11391268_10204499347709285_3889169831865952197_n.jpg?oh=7f3cf692d4dca830c835e0c9024dbc96&oe=55F4D3D2&__gda__=1442616678_b8b15ac6a8b75419a593cc611e190e95"
+  },
+  {
+    "id" : 19,
+    "title" : "Dolor sit",
+    "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget justo mattis ipsum finibus fringilla quis non eros. Donec nunc magna, maximus non eros quis, dignissim dapibus quam. Donec hendrerit sapien quis ex feugiat, nec rutrum elit pharetra. Phasellus tempor eleifend elementum. Fusce posuere nec nunc eget pretium. Morbi nec posuere elit, sed sollicitudin elit. Quisque et mauris ac elit rutrum maximus vehicula quis risus. Donec sed posuere diam.",
+    "url" : "https://fbcdn-sphotos-b-a.akamaihd.net/hphotos-ak-xft1/v/t1.0-9/11390378_10204499347029268_9202303841252452016_n.jpg?oh=fe8a27b81282af40fd6103e637d6e7a7&oe=56066274&__gda__=1443172486_44f574be25879517ecee37c62ecd6f05"
+  },
+  {
+    "id" : 20,
+    "title" : "Dolor sit",
+    "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget justo mattis ipsum finibus fringilla quis non eros. Donec nunc magna, maximus non eros quis, dignissim dapibus quam. Donec hendrerit sapien quis ex feugiat, nec rutrum elit pharetra. Phasellus tempor eleifend elementum. Fusce posuere nec nunc eget pretium. Morbi nec posuere elit, sed sollicitudin elit. Quisque et mauris ac elit rutrum maximus vehicula quis risus. Donec sed posuere diam.",
+    "url" : "https://fbcdn-sphotos-a-a.akamaihd.net/hphotos-ak-xpt1/t31.0-8/s720x720/1556283_10204499345669234_8621539168670781182_o.jpg"
+  }
+];
+
+
+
+var miObjeto = {nombre: "Felix", apellido: "Izarra"};
+var imgLink = "http://www.ciens.ucv.ve/portalasig/assets/ucv1-26b091ec1bda4df55e1e25c607d66b2e.gif";
+var newTR = ""
+var appear = true;
+var elementCounter = 0;
+
+function Alerta()
+{
+	var miContent = document.getElementById("Content");
+	miContent.innerHTML += miObjeto["nombre"];
+	console.log("loquesea");
+}
+
+function getDocHeight()
+{
+	var D = document;
+	return Math.max(
+			D.body.scrollHeight, D.documentElement.scrollHeight,
+			D.body.offsetHeight, D.documentElement.offsetHeight,
+			D.body.clientHeight, D.documentElement.clientHeight
+		);
+}
+
+$(document).ready(function(){
+
+		$(window).scroll(function(){
+			if($(window).scrollTop() + $(window).height() == getDocHeight())
+			{
+				//alert("Bottom!");
+				fill();
+			}
+		})
+
+});
+
+function fill(){
+
+				var openTable = "<tr>", closeTable = "</tr>";
+				var newEntry = "";
+				for (var i = 1; i < 3; i++)
+				 {
+						 var elename = "ele"+elementCounter;
+						newEntry += "<td id=" + elename + "<p>"	+misObjetos[elementCounter]["title"]+"</p>";
+						newEntry +="<img src="+misObjetos[elementCounter]["url"]+">";
+						newEntry += "<div><h4>"+misObjetos[elementCounter]["description"]+"</h4></div>";
+						newEntry += "</td>";
+						elementCounter++;
+						
+				 }
+				$("table").append(openTable + newEntry + closeTable);
+
+				var newEntry = "";
+				var elename = "ele"+elementCounter;
+				var middleStr = "middle";
+				newEntry += "<td id=\"middle\" <p>"	+misObjetos[elementCounter]["title"]+"</p>";
+				newEntry +="<img src="+misObjetos[elementCounter]["url"]+">";
+				newEntry += "<div><h4>"+misObjetos[elementCounter]["description"]+"</h4></div>";
+				newEntry += "</td>";
+				elementCounter++;
+				$("table").append(openTable + newEntry + closeTable);
+
+				var newEntry = "";
+				for (var i = 1; i < 3; i++)
+				 {
+						var elename = "ele"+elementCounter;
+						newEntry += "<td id=" + elename + "<p>"	+misObjetos[elementCounter]["title"]+"</p>";
+						newEntry +="<img src="+misObjetos[elementCounter]["url"]+">";
+						newEntry += "<div><h4>"+misObjetos[elementCounter]["description"]+"</h4></div>";
+						newEntry += "</td>";
+						elementCounter++;
+				 }
+				$("table").append(openTable + newEntry + closeTable);
+}
+
+$(document).ready(function(){
+
+		$("button").click(fill());
+});
+
