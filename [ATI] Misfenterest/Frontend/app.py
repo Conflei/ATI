0a37725e83fd1326285d53655fd86e80083d7ab0
Conflei <paramaplexd@gmail.com>
2015-08-01 16:53:56 -0430

    Cambio de directorios

---
+++ [ATI] Misfenterest/Frontend/app.py
@@ -0,0 +1,40 @@
+from flask import *
+
+app = Flask (__name__)
+#, template_folder = 'views', static_folder = 'statics')
+
+# Routes goes here
+
+@app.route('/')
+def index():
+	return 'Hello Flask'
+
+@app.route('/sayhello')
+def pedro():
+	name = request.args.get('name')
+	return render_template('say.html', name = name)
+
+@app.route('/mylogin')
+def mylogin():
+	name = request.args.get('name')
+	return render_template('../../Frontend/index.html', name = name)
+
+@app.route('/form')
+def xs():
+	return render_template('form.html')
+
+@app.route('/login', methods = ['POST'])
+def login():
+	email = request.form['Email']
+	password = request.form['Password']
+	if(email == 'pedro@gmail.com'and password == '1'):
+		return render_template('say.html', name = email)
+	return render_template('form.html')
+
+# Routes end here
+
+if __name__ == '__main__':
+  app.debug = True
+  app.run( host = '0.0.0.0', port = 5000 )
+
+
